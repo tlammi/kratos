f71a9ce68e00b8a79c74dc6c4d95ea6a1290a306
Toni Lammi <tangojulietlima@gmail.com>
2019-12-02 22:04:15 +0200

    Added a script for running pylint (#12)
    Closes #9

--- script/run_pylint.py
+++ script/run_pylint.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+"""
+A script for running pylint
+"""
+import os
+import sys
+import pylint
 
-if __name__ == "__main__":
-	print("TODO: Implement")
+
+THISDIR = os.path.dirname(os.path.realpath(__file__))
+RCFILE = os.path.join(THISDIR, "pylintrc")
+PYLINT_ARGS = ["--rcfile=%s" % RCFILE]
+
+# Modify path to get the import checks right
+sys.path.insert(0, os.path.join(THISDIR, "../kratos"))
+
+if len(sys.argv) > 1:
+    ARGS = sys.argv[1:]
+else:
+    ARGS = [os.getcwd()]
+
+sys.exit(pylint.lint.Run(PYLINT_ARGS + ARGS))
