7437bbe73dead6d8217c24580d995fc119b3e32f
Iiro Vuorio <iiro.vuorio@gmail.com>
2020-02-05 06:34:42 +0200

    Larger changes based on review

--- kratos/unit/platform.py
+++ kratos/unit/platform.py
@@ -5,46 +5,47 @@ This unit also handles attempt time management and displays the
 judging results.
 """
 import argparse
+import dataclasses
 import enum
 import time
 
 import mqttengine as mqtt
 import gui
-import util.timer as timer
+import util.timer
 
-
-UNIT_NAME = "platform"
-
-clock_start_value = 60
-clock_timer = timer.Timer()
 
 class LiftJudging(enum.Enum):
     NOT_VALID = enum.auto()
     GOOD_LIFT = "white"
     NOT_GOOD_LIFT = "red"
 
-judgings = {
-    "left": LiftJudging.NOT_VALID,
-    "middle": LiftJudging.NOT_VALID,
-    "right": LiftJudging.NOT_VALID
-}
+@dataclasses.dataclass
+class Judgings:
+    left: LiftJudging
+    middle: LiftJudging
+    right: LiftJudging
+
+@dataclasses.dataclass
+class ClockTimer:
+    timer: util.timer.Timer
+    start_value: int
 
 
-def parse_float_or(string: str, default: float=None) -> float:
+UNIT_NAME = "platform"
+
+def parse_float_or(string: str, default: float = None) -> float:
     try:
         res = float(string)
-    except:
+    except ValueError:
         res = default or 0.0
-    finally:
-        return res
+    return res
 
-def parse_int_or(string: str, default: int=None) -> int:
+def parse_int_or(string: str, default: int = None) -> int:
     try:
         res = int(string)
-    except:
+    except ValueError:
         res = int(parse_float_or(string, default))
-    finally:
-        return res
+    return res
 
 def set_clock_time(screen_data_handle: gui.PlatformData,
                    seconds: int):
@@ -52,18 +53,18 @@ def set_clock_time(screen_data_handle: gui.PlatformData,
     screen_data_handle.minutes = minutes
     screen_data_handle.seconds = seconds - minutes*60
 
-def convert_judging(judging: LiftJudging) -> bool:
-    if judging == LiftJudging.NOT_VALID:
-        return None
-    elif judging == LiftJudging.GOOD_LIFT:
-        return True
-    elif judging == LiftJudging.NOT_GOOD_LIFT:
-        return False
+def convert_judging(judging: LiftJudging) -> str:
+    res = "NA"
+    if judging == LiftJudging.GOOD_LIFT:
+        res = "white"
+    if judging == LiftJudging.NOT_GOOD_LIFT:
+        res = "red"
+    return res
 
 
 @mqtt.topic_handler("lifter/current/#", UNIT_NAME)
-def lifter_handler(client, userdata, msg):
-    screen_data = userdata
+def lifter_handler(_, userdata, msg):
+    screen_data, _, _ = userdata
 
     topic = msg.topic.replace("lifter/current/", "")
     path = topic.split("/")
@@ -84,8 +85,7 @@ def lifter_handler(client, userdata, msg):
 
 @mqtt.topic_handler("clock/#", UNIT_NAME)
 def clock_handler(client, userdata, msg):
-    global clock_timer, clock_start_value
-    screen_data = userdata
+    screen_data, clock, _ = userdata
 
     def once_per_second(remaining):
         nonlocal screen_data
@@ -104,63 +104,62 @@ def clock_handler(client, userdata, msg):
             screen_data.seconds = 0
             time.sleep(0.3)
 
-        set_clock_time(screen_data, clock_start_value)
+        set_clock_time(screen_data, clock.start_value)
 
     topic = msg.topic.replace("clock/", "")
     path = topic.split("/")
 
     if path[0] == "start":
-        if not clock_timer.running:
-            countdown_value = (clock_timer.remaining_time if clock_timer.remaining_time > 0
-                                                          else clock_start_value)
-            clock_timer.start(countdown_value, once_per_second, expired)
+        if not clock.timer.running:
+            countdown_value = (clock.timer.remaining_time if clock.timer.remaining_time > 0
+                               else clock.start_value)
+            clock.timer.start(countdown_value, once_per_second, expired)
     elif path[0] == "stop":
-        if clock_timer.running:
-            clock_timer.stop()
+        if clock.timer.running:
+            clock.timer.stop()
     elif path[0] == "time":
         if path[1] == "set":
-            if clock_timer.running:
+            if clock.timer.running:
                 return
             value = parse_int_or(msg.payload, 0)
             if value > 0:
-                clock_start_value = value
-                set_clock_time(screen_data, clock_start_value)
-                clock_timer.clear()
+                clock.start_value = value
+                set_clock_time(screen_data, clock.start_value)
+                clock.timer.clear()
         elif path[1] == "refresh":
-            value = (clock_timer.remaining_time if clock_timer.remaining_time > 0
-                                                else clock_start_value)
+            value = (clock.timer.remaining_time if clock.timer.remaining_time > 0
+                     else clock.start_value)
             client.publish("clock/time/current", value)
 
 @mqtt.topic_handler("judge/vote/#", UNIT_NAME)
-def voting_result_handler(client, userdata, msg):
-    global judgings
-    screen_data = userdata
+def voting_result_handler(_, userdata, msg):
+    screen_data, _, judgings = userdata
 
-    topic = msg.topic.replace("judge/vote/", "")
-    if topic in judgings:
+    position = msg.topic.replace("judge/vote/", "")
+    if position in ("left", "middle", "right"):
         color = msg.payload.decode("utf-8")
         if color in ("red", "white"):
-            judgings[topic] = LiftJudging(color)
+            setattr(judgings, position, LiftJudging(color))
         else:
             return
 
     red, white = (0, 0)
-    for judging in judgings.values():
-        if judging == LiftJudging.GOOD_LIFT:
+    for position in ("left", "middle", "right"):
+        if getattr(judgings, position) == LiftJudging.GOOD_LIFT:
             white += 1
-        elif judging == LiftJudging.NOT_GOOD_LIFT:
+        elif getattr(judgings, position) == LiftJudging.NOT_GOOD_LIFT:
             red += 1
 
     if red >= 2 or white >= 2:
-        screen_data.set_judging(left=convert_judging(judgings["left"]),
-                                middle=convert_judging(judgings["middle"]),
-                                right=convert_judging(judgings["right"]))
+        screen_data.set_judging(left=convert_judging(judgings.left),
+                                middle=convert_judging(judgings.middle),
+                                right=convert_judging(judgings.right))
 
         if red + white == 3:
             time.sleep(3)
             screen_data.clear_judging()
-            for judging in judgings.values():
-                judging = LiftJudging.NOT_VALID
+            for position in ("left", "middle", "right"):
+                setattr(judgings, position, LiftJudging.NOT_VALID)
 
 
 def add_cli_args(parser: argparse.ArgumentParser) -> None:
@@ -170,9 +169,9 @@ def add_cli_args(parser: argparse.ArgumentParser) -> None:
     :param parser: cli parser
     """
 
-    parser.add_argument("--virtual",
-                        help="Specifiying virtual launches the GUI in a window",
-                        action="store_true")
+    parser.add_argument("-w", "--window",
+                        help="Choose the type of window to launch",
+                        metavar="[window, framebuffer]")
 
 def run(args: argparse.Namespace) -> None:
     """
@@ -183,15 +182,24 @@ def run(args: argparse.Namespace) -> None:
     :param args: arguments passed via cli
     """
 
-    if args.virtual:
+    if args.window == "window":
         screen = gui.VirtualPlatformScreen()
-    else:
+    elif args.window == "framebuffer":
         screen = gui.PlatformScreen()
+    else:
+        raise RuntimeError("Invalid window type")
 
-    set_clock_time(screen.data, clock_start_value)
+    judgings = Judgings(left=LiftJudging.NOT_VALID,
+                        middle=LiftJudging.NOT_VALID,
+                        right=LiftJudging.NOT_VALID)
+    clock = ClockTimer(timer=util.timer.Timer(), start_value=60)
+
+    set_clock_time(screen.data, clock.start_value)
 
     mqtt.engine().configure(mqtt.MqttEngineConfig(UNIT_NAME,
-                                                  screen.data))
+                                                  (screen.data,
+                                                   clock,
+                                                   judgings)))
     mqtt.engine().connect(args.address, args.port, bind_addr=args.bind)
     mqtt.engine().start()
     screen.run()
