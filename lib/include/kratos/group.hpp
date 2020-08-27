ee739a96c2e64e8daa74d3a3eaa9e852272a649d
Toni Lammi <toni.lammi@wapice.com>
2020-08-27 10:46:20 +0300

    More scething

---
+++ lib/include/kratos/group.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace kratos{
+
+struct Group{};
+}
--- lib/include/kratos/kratos.hpp
+++ lib/include/kratos/kratos.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "kratos/detail/data.hpp"
 #include "kratos/api/competition_api.hpp"
 #include "kratos/api/group_api.hpp"
+#include "kratos/group.hpp"
+#include "kratos/competitor.hpp"
 
